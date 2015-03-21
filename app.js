f2ea40bc77f0d41a93b6c04ac999e345c11d4a34
Michael Haase <darkstar@gernox.de>
2015-03-21 11:58:30 +0100

    First try to retrieve and delete a message from SQS

--- app.js
+++ app.js
@@ -25,6 +25,40 @@ aws.config.update({
  */
 var worker = function() {
     console.log('Start worker...');
+
+    var sqs = new aws.SQS();
+
+    var params = {
+        QueueUrl: conf.get('AWS:SQS:QueueUrl'),
+        AttributeNames: [
+            'All'
+        ],
+        MaxNumberOfMessages: 1,
+        VisibilityTimeout: 0,
+        WaitTimeSeconds: 20
+    };
+
+    sqs.receiveMessage(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else {
+            console.log(data);           // successful response
+
+            if (data.Messages) {
+                var message = data.Messages[0];
+                var receiptHandle = message.ReceiptHandle;
+
+                var deleteParams = {
+                    QueueUrl: conf.get('AWS:SQS:QueueUrl'),
+                    ReceiptHandle: receiptHandle
+                };
+
+                sqs.deleteMessage(deleteParams, function(err, data) {
+                    if (err) console.log(err, err.stack); // an error occurred
+                    else     console.log(data);           // successful response
+                });
+            }
+        }
+    });
 };
 
 worker();

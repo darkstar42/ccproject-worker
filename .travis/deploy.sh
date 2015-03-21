81ad97ebccac60784766d2945bb604bf86ad601b
Michael Haase <darkstar@gernox.de>
2015-03-21 20:20:15 +0100

    Add init script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,9 @@ if [[ $TRAVIS_BRANCH == 'production' ]]; then
     rm -Rf /tmp/deploy
     mkdir -p /tmp/deploy/opt/ccproject-worker
     cp -pR ./* /tmp/deploy/opt/ccproject-worker
+    mkdir -p /tmp/deploy/opt/etc/init.d/
+    cp .travis/ccproject-worker /tmp/deploy/opt/etc/init.d/ccproject-worker
+    chmod +x /tmp/deploy/opt/etc/init.d/ccproject-worker
     rm -f *.deb
     fpm -s dir -t deb -C /tmp/deploy --name ccproject-worker --version 0.0.1 --iteration build-$TRAVIS_BUILD_NUMBER .
 

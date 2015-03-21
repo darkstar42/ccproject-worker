e02411140c2cc183a9979151e1f7f0f60832e092
Michael Haase <darkstar@gernox.de>
2015-03-21 19:53:37 +0100

    Add travis ci stuff

---
+++ .travis/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $TRAVIS_BRANCH == 'production' ]]; then
+    rm -Rf /tmp/deploy
+    mkdir -p /tmp/deploy/opt/ccproject-worker
+    cp -pR ./* /tmp/deploy/opt/ccproject-worker
+    rm -f *.deb
+    fpm -s dir -t deb -C /tmp/deploy --name ccproject-worker --version 0.0.1 --iteration build-$TRAVIS_BUILD_NUMBER .
+
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null *.deb deployment@puppet.cc.gernox.de:/tmp/
+fi
